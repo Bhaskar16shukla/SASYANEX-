6fe7f87d746f9f8cea3343dbc4d10adb2e79e203
Bhaskar16shukla <bhaskar16shukla@gmail.com>
2025-10-06 12:52:43 +0530

    Delete latest website directory

--- latest website/chat.js
+++
@@ -1,6 +0,0 @@
-export function openChatBox() {
-    document.getElementById('chat-box').style.display = 'flex';
-}
-export function closeChatBox() {
-    document.getElementById('chat-box').style.display = 'none';
-}
